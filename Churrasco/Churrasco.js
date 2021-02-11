685cbe8131e5ccfebb70bd74fe7fb473f14492a4
JacobLeoPereira1910 <jpsleonardo@icloud.com>
2021-02-11 02:23:50 -0300

    Projeto de site para estudos de HTML5 e CSS3 entre outras linguagens de programação

---
+++ Churrasco/Churrasco.js
@@ -0,0 +1,20 @@
+/**
+ * Churrasculadora
+ * @author Yago Aparecido dos Santos Silva
+ */
+
+ function calcularChurras(){
+     let KGCarne, Cerveja, Refrigerante, Homens, Mulheres, Crianças, TotalCarne, TotalCerveja, TotalRefrigerante, DiviaoHeM
+     Crianças = parseFloat(frmChurrasco.valorCrianças.value.replace(",","."))
+     Mulheres = parseFloat(frmChurrasco.valorMulheres.value.replace(",","."))
+     Homens = parseFloat(frmChurrasco.valorHomens.value.replace(",","."))
+     KGCarne = parseFloat(frmChurrasco.valorKGCarne.value.replace(",","."))
+     Refrigerante = parseFloat(frmChurrasco.valorRefrigerante.value.replace(",","."))
+     Cerveja = parseFloat(frmChurrasco.valorCerveja.value.replace(",","."))
+
+
+
+
+
+     
+ }
---
+++ appFlex/calcflex.js
@@ -0,0 +1,24 @@
+/**
+ * Calculadora Flex
+ * @author Leonardo Jacob
+ *
+ */
+
+let etanol, gasolina
+
+function Calcular() {
+    etanol = frmFlex.txtEtanol.value.replace(",", ".")
+    gasolina = frmFlex.txtGasolina.value.replace(",", ".")
+
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById("status").src = "etanol.png"
+    }
+    else {
+        document.getElementById("status").src = "gasolina.png"
+    }
+}
+
+
+function Resetar() {
+    document.getElementById("status").src = "neutro.png"
+}
